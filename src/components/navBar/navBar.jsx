cc1347a7ae91276be7faf025a0af6989e8217aad
Nathan Yocum <nayocum@nevada.unr.edu>
2019-02-22 16:57:51 -0800

    Added Axios and learned how to make requests to proxy server

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 import { NavBarStyle, UnstyledButton } from '../__styles__/styles';
 
-const NavBar = () => (
-  <NavBarStyle>
-    <UnstyledButton onClick={() => {}}>
-      <FontAwesomeIcon size="lg" icon="bars" color="#e0e0e0" />
-    </UnstyledButton>
-    <div style={{ flexGrow: 2, textAlign: 'center' }}>ti-graph</div>
-  </NavBarStyle>
-);
+const NavBar = () => {
+  const [apiData, setAPIData] = React.useState(null);
+
+  axios
+    .get('/api')
+    .then(({ data }) => {
+      setAPIData(data);
+      return data;
+    })
+    .catch(err => console.error(err));
+  return (
+    <NavBarStyle>
+      <UnstyledButton onClick={() => {}}>
+        <FontAwesomeIcon size="lg" icon="bars" color="#e0e0e0" />
+      </UnstyledButton>
+      <div style={{ flexGrow: 2, textAlign: 'center' }}>ti-graph</div>
+      {apiData !== null && apiData.bar}
+    </NavBarStyle>
+  );
+};
 
 export default NavBar;
