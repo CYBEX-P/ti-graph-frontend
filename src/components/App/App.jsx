aab36b80b1bcace4b0b8b446670931e27926651f
Nathan Yocum <nayocum@nevada.unr.edu>
2019-02-28 09:42:10 -0800

    Added styles for children on the menubars

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,13 +15,23 @@ const App = () => {
     <MenuContext.Provider value={{ isExpanded, dispatchExpand }}>
       <AppContainer>
         {/* Important to have content rendered under navbar and menubar */}
-        <ContentContainerStyle>
+        <ContentContainerStyle
+          onClick={() => {
+            dispatchExpand('none');
+          }}
+        >
           <p>Graph will go here!</p>
         </ContentContainerStyle>
         <NavBar />
-        <MenuBar side="left" icon="search" />
-        <MenuBar side="right" icon="history" />
-        <MenuBar side="bottom" icon="list" />
+        <MenuBar side="left" icon="search">
+          <button type="button">Press to make a modal appear</button>
+        </MenuBar>
+        <MenuBar side="right" icon="history">
+          <div>Hello</div>
+        </MenuBar>
+        <MenuBar side="bottom" icon="list">
+          <div>Hello</div>
+        </MenuBar>
       </AppContainer>
     </MenuContext.Provider>
   );
