b54ec21d032af19cb7633f0943234034026850f5
nyocum <nayocum@nevada.unr.edu>
2019-04-15 12:06:03 -0700

    More config support for easier composability

--- src/components/radialMenu/withNodeType.jsx
+++ src/components/radialMenu/withNodeType.jsx
@@ -39,14 +39,16 @@ function withNodeType(RadialMenuComponent, nodeType, setNeo4jData, config) {
   let onClickFns = [];
   let titles = [];
   if (Object.keys(nodeType)[0] === 'IP') {
-    icons = ['server', 'globe', 'project-diagram', 'user', 'passport', 'plus-circle'];
     // We could probably find a way to do this by YAML instead of hardcoding it
     onClickFns = config.enrichments.SrcIP.map(enrichmentType => () => {
       return EnrichIPbyType(enrichmentType);
     });
-    titles = config.enrichments.SrcIP;
-    onClickFns.push(() => EnrichIPAll());
+    // Copy arrays
+    titles = config.enrichments.SrcIP.map(val => val);
+    icons = config.enrichments.SrcIP.map(val => val);
     titles.push('all');
+    icons.push('all');
+    onClickFns.push(() => EnrichIPAll());
   }
   return props => {
     return <RadialMenuComponent titles={titles} icons={icons} onClickFunctions={onClickFns} {...props} />;
