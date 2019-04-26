c538d9772322f2b55878c5e90eb1dc3b73b99fe0
Nathan Yocum <nayocum@nevada.unr.edu>
2019-04-26 12:20:01 -0700

    Landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import MainApp from './components/App/MainApp';
@@ -9,6 +9,7 @@ import Find from './pages/Find';
 import Found from './pages/Found';
 import Remove from './pages/Remove';
 import Update from './pages/Update';
+import Landing from './pages/Landing';
 import NavBar from './components/navBar/navBar';
 import MenuContext from './components/App/MenuContext';
 
@@ -19,14 +20,34 @@ const App = ({ config }) => {
     }
     return 'none';
   }, 'none');
+  const [isSignedIn, setSignedIn] = useState(() => {
+    if (localStorage.getItem('token')) {
+      return true;
+    }
+    return false;
+  });
+
+  // For now, set a local storage token to anything to see logged in behavior
+  // useEffect(() => localStorage.setItem('token', 'hi'));
+
+  useEffect(() => {
+    // TODO: Change this to be whatever we decide to save the token as
+    if (localStorage.getItem('token')) {
+      // TODO: Change to actual authentication istead of just being if the token exists
+      return setSignedIn(true);
+    }
+    return setSignedIn(false);
+  }, []);
   return (
     <Router>
       <div style={{ minHeight: '100vh', backgroundColor: '#efefef' }} className="App">
-        <Route exact path="/" component={() => <MainApp config={config} />} />
+        <Route exact path="/ti-graph" component={() => <MainApp config={config} />} />
         <MenuContext.Provider value={{ dispatchExpand, isExpanded }}>
           <NavBar />
         </MenuContext.Provider>
         <div style={{ backgroundColor: '#ffffff', paddingTop: '56px', paddingBottom: '32px' }} className="container">
+          <Route exact path="/" component={() => <Landing isSignedIn={isSignedIn} />} />
+          <Route exact path="/home" component={() => <Landing isSignedIn={isSignedIn} />} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/profile" component={Profile} />
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from '../components/Button/Button';
 
-const Landing = () => {
-  return (
-    <div className="container">
-      <div className="jumbotron mt-5">
-        <div className="col-sm-8 mx-auto">
-          <h1 className="text-center"> WELCOME </h1>
-        </div>
-      </div>
-    </div>
-  );
-};
+const Landing = ({ isSignedIn }) => (
+  <>
+    <h1 className="text-center">Threat Intelligence Graph</h1>
+    <p>NSF funded project at the University of Nevada, Reno</p>
+    {!isSignedIn && (
+      <>
+        <a href="/ti-graph">
+          <Button hasIcon width="100%" onClickFunction={() => {}}>
+            <FontAwesomeIcon fixedWidth size="lg" icon="user" color="#e0e0e0" />
+            <div style={{ width: '100%', textAlign: 'center' }}>Login</div>
+          </Button>
+        </a>
+        <br />
+        <a href="/ti-graph">
+          <Button hasIcon width="100%" onClickFunction={() => {}}>
+            <FontAwesomeIcon fixedWidth size="lg" icon="user-plus" color="#e0e0e0" />
+            <div style={{ width: '100%', textAlign: 'center' }}>Register</div>
+          </Button>
+        </a>
+      </>
+    )}
+    {isSignedIn && (
+      <>
+        <a href="/logout">Logout</a>
+        <a href="/ti-graph">
+          <Button width="100%" onClickFunction={() => {}}>
+            <div style={{ width: '100%', textAlign: 'center' }}>Gaph</div>
+          </Button>
+        </a>
+      </>
+    )}
+  </>
+);
 
 export default Landing;
