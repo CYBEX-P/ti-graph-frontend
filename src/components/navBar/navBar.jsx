5a676e483dbdc772e95a34201437e50a92eb956c
Nathan Yocum <nayocum@nevada.unr.edu>
2019-04-26 12:32:02 -0700

    Update link

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
         >
           <FontAwesomeIcon size="lg" icon="bars" color="#e0e0e0" />
         </UnstyledButton>
-        <a style={{ flexGrow: 2, textAlign: 'center', color: '#e3e3e3' }} href="/">
+        <a style={{ flexGrow: 2, textAlign: 'center', color: '#e3e3e3' }} href="/ti-graph">
           <div>ti-graph</div>
         </a>
         <UnstyledButton onClick={() => {}}>
